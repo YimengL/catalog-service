5e7544612cf0a0f79380a9f3097d1757b826059a
yimengli <yimengcsu@gmail.com>
2025-07-06 01:37:30 +0200

    cnsia - ch05 - Update BookRepository

--- src/main/java/com/polarbookshop/catalog_service/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalog_service/domain/BookRepository.java
@@ -1,11 +1,21 @@
 package com.polarbookshop.catalog_service.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
-public interface BookRepository {
-    Iterable<Book> findAll();
+// Extends a repository providing CRUD operations, specifying the type of managed entity (Book) and its primary key type (Long).
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    // Methods implemented by Spring Data at runtime
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
-    Book save(Book book);
+
+    @Modifying // Identifies an operation that will modify the database state
+    @Transactional // Identifies the method to be executed in a transaction
+    @Query("delete from Book where isbn = :isbn") // Declares the query that Spring Data will use to implement the method
     void deleteByIsbn(String isbn);
 }
