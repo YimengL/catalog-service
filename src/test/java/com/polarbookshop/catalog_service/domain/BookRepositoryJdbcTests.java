cab431f8d82ae7c031867f70806ad4e65b991a49
yimengli <yimengcsu@gmail.com>
2025-07-06 13:22:46 +0200

    cnsia - ch05 - Add unit test BookRepositoryJdbcTests

---
+++ src/test/java/com/polarbookshop/catalog_service/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,39 @@
+package com.polarbookshop.catalog_service.domain;
+
+
+import com.polarbookshop.catalog_service.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest // Identifies a test class that focus on Spring Data JDBC components
+@Import(DataConfig.class) // Imports the data configuration (needed to enable auditing)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Disable the default behavior of relying on an embedded test database since we want to use Testcontainers
+@ActiveProfiles("integration") // Enables the "integration" profile to load configuration from application-integration.yml
+public class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate; // A lower-level object to interact with the database
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book); // JdbcAggregateTemplate is used to prepare the data targeted by the test
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
