d3a12f00bb34356814b41751a927f3ef9804f405
yimengli <yimengcsu@gmail.com>
2025-07-06 13:19:15 +0200

    cnsia - ch05 - Fix integration test

--- src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -4,12 +4,14 @@ import com.polarbookshop.catalog_service.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // Loads a full Spring web application context and a Servlet container listening on a random port
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration") // Enables the "integration" profile to load configuration from application-integration.yml
 class CatalogServiceApplicationTests {
 
 	// Utility to perform REST calls for testing
@@ -18,7 +20,7 @@ class CatalogServiceApplicationTests {
 
 	@Test
 	void whenPostRequestThenBookCreated() {
-		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
 
 		webTestClient
 				.post()
--- src/test/java/com/polarbookshop/catalog_service/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalog_service/web/BookJsonTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,8 +19,11 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+        var now = Instant.now();
+        var book = new Book(394L, "1234567890", "Title", "Author", 9.90, now, now,  21);
         var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(book.id().intValue());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title")
@@ -28,20 +32,31 @@ public class BookJsonTests {
                 .isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdDate")
+                .isEqualTo(book.createdDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate")
+                .isEqualTo(book.lastModifiedDate().toString());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
+                .isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
+        var instant = Instant.parse("2025-07-01T10:40:26.246118Z");
         var content = """
         {
+            "id": 394,
             "isbn": "1234567890",
             "title": "Title",
             "author": "Author",
-            "price": 9.90
+            "price": 9.90,
+            "createdDate": "2025-07-01T10:40:26.246118Z",
+            "lastModifiedDate": "2025-07-01T10:40:26.246118Z",
+            "version": 21
         }
         """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book("1234567890", "Title", "Author", 9.90));
+                .isEqualTo(new Book(394L, "1234567890", "Title", "Author", 9.90, instant, instant,21));
     }
 }
