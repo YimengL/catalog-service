2c37be8b020c7a4ab5ca7e35870408c6031f392e
yimengli <yimengcsu@gmail.com>
2025-07-06 17:41:54 +0200

    cnsia - ch05 - Add BookServiceTest

--- src/main/java/com/polarbookshop/catalog_service/domain/BookNotFoundException.java
+++ src/main/java/com/polarbookshop/catalog_service/domain/BookNotFoundException.java
@@ -3,6 +3,6 @@ package com.polarbookshop.catalog_service.domain;
 public class BookNotFoundException extends RuntimeException {
 
     public BookNotFoundException(String isbn) {
-        super("Book with ISBN " + isbn + " not found");
+        super("The book with ISBN " + isbn + " was not found.");
     }
 }
